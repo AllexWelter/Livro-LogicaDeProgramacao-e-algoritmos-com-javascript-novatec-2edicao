ca297fa900d33b0ecc705401afee781ce5f99337
Allex Welter <allexwelter13@gmail.com>
2024-11-17 09:20:28 -0300

    template resp4_d

---
+++ cap04/js/resp4_d.js
@@ -0,0 +1,6 @@
+/*Elaborar um programa que leia três lados e verifique se eles podem ou não formar um triângulo. Para formar um triângulo, um dos lados não pode ser maior que a soma dos outros 2. Caso possam formar um triângulo, exiba também qual o tipo do triângulo:
+Equilátero -> 3 lados iguais
+Isósceles -> 2 lados iguais
+Escaleno -> 3 lados diferentes
+*/
+
