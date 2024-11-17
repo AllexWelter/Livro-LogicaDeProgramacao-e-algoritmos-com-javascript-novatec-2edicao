a36f9ab4f764ffe8b58afeda532531f1762ae3dd
Allex Welter <allexwelter13@gmail.com>
2024-11-17 06:36:26 -0300

    finish 4_a

--- cap04/js/resp4_a.js
+++ cap04/js/resp4_a.js
@@ -1,2 +1,32 @@
 //Elaborar um programa que leia números. Informe se ele é par ou ímpar. Faça com o if...else... tradicional e, após, tente criar a condição com o operador ternário.
 
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const num = Number(frm.inNumero.value)
+    //OPERADOR TERNÁRIO
+    let calc = num % 2 == 0 ? "Par" : "Ímpar"
+    resp.innerText = `${num} é ${calc}`   
+})
+
+
+
+
+
+
+
+
+
+
+/*
+IF E ELSE 
+const num = Number(frm.inNumero.value)
+    if(num % 2 == 0) {
+        resp.innerText = `${num} é Par`
+    } else {
+        resp.innerText = `${num} é Ímpar `
+    }
+
+*/
