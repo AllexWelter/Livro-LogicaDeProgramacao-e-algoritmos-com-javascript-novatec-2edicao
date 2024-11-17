41af61293a4c1852e3be384c79d31c2694d7e612
Allex Welter <allexwelter13@gmail.com>
2024-11-17 07:09:14 -0300

    update resp4_b

---
+++ cap04/js/resp4_b.js
@@ -0,0 +1,3 @@
+//Elaborar um programa que leia a velocidade permitida em uma estrada e a velocidade de um condutor. Se a velocidade for inferior ou igual à permitida, exibe "Sem Multa". Se a velocidade for de até 20% maior que a permitida, exiba "Multa Leve". E, se a velocidade for superior a 20% da velocidade permitida, exiba "Multa Grave".
+
+
