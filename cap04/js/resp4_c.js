104614b3e9dbf035b2d5039c49bf519470419fa3
Allex Welter <allexwelter13@gmail.com>
2024-11-17 08:21:46 -0300

    update resp4_c

---
+++ cap04/js/resp4_c.js
@@ -0,0 +1,8 @@
+//Elaborar um programa para simular um parquímetro, o qual leia o valor de moedas depositado em um terminal de estacionamento rotativo. O programa deve informar o tempo de permanência do veículo no local e o troco (se existir). Se o valor for inferior ao tempo mínimo, exiba a mensagem "Valor insulficiente".
+/*
+valor         tempo(min)
+1,00          30
+1,75          60
+3,00          120
+*/
+
