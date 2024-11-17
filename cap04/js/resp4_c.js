ac145575ece2f5f0a6e4f3fc749d7a7bc08752f3
Allex Welter <allexwelter13@gmail.com>
2024-11-17 09:07:20 -0300

    finish 4.c

--- cap04/js/resp4_c.js
+++ cap04/js/resp4_c.js
@@ -6,3 +6,38 @@ valor         tempo(min)
 3,00          120
 */
 
+
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const valor = Number(frm.inValor.value)
+    if (valor < 1.00) {
+        alert("Valor insulficiente, insira no mínimo R$1,00")
+        frm.inValor.focus()
+        return
+    }
+
+    let tempo
+    let troco
+    resp2.innerText = ``
+    
+    if (valor >= 3) {
+        tempo = 120
+        troco = valor - 3.00
+    } else if (valor >= 1.75) {
+        tempo = 60
+        troco = valor - 1.75
+    } else {
+        tempo = 30
+        troco = valor - 1.00
+    }
+
+    resp1.innerText = `Tempo: ${tempo} min`
+    if (troco > 0) {
+        resp2.innerText = `Troco R$: ${troco.toFixed(2)}`
+    }
+    
+})
