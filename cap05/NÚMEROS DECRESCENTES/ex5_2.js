1aea7c3ef4f1453240a9016176c2197e3c6fd084
Allex Welter <allexwelter13@gmail.com>
2024-11-18 08:49:48 -0300

    end ex5_2

--- cap05/NÚMEROS DECRESCENTES/ex5_2.js
+++ cap05/NÚMEROS DECRESCENTES/ex5_2.js
@@ -1,13 +1,16 @@
 const frm = document.querySelector("form") //obtém elementos da página
 const resp = document.querySelector("h3")
 
-frm.addEventListener("submit", (e) => {    
+frm.addEventListener("submit", (e) => {
     e.preventDefault()              //evita envio do form
-    
+
     const numero = Number(frm.inNumero.value)  //obtém número informado
     let resposta = `Entre ${numero} e 1: `    //string para montar a resposta
-    for (let i = numero; i > 0; i--) {     //cria um for decrescente
-        resposta = resposta + i + ","       //resposta acumula números (e vírgulas)
-    }       
-    resp.innerText = resposta         //exibe a resposta
+    for (let i = numero; i > 1; i--) {     //cria um for decrescente
+        resposta = `${resposta} ${i},`     //resposta acumula números (e vírgulas) pode ser também -> //resposta + i + ","
+    }
+    resp.innerText = resposta + " 1"        //exibe a resposta
 })
+
+
+
