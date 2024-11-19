f9bada98129bb4a27bca8ba56db404540de5a66d
Allex Welter <allexwelter13@gmail.com>
2024-11-19 08:40:38 -0300

    finish ok ex5_5

--- cap05/CONTAS DO MÊS/ex5_5.js
+++ cap05/CONTAS DO MÊS/ex5_5.js
@@ -14,7 +14,7 @@ frm.addEventListener("submit", (e) => {       //"escuta" evento submit do form
     
     numContas++                              //adiciona valores ao contador e acumulaodr
     valTotal = valTotal + valor             // ou: valTotal += valor
-    resposta = resposta + descricao + " - R$: " + valor.toFixed(2) + "\n"
+    resposta = resposta + descricao + " - R$: " + valor.toFixed(2) + "\n"    // pode ser feito por template string -> resposta = `${resposta} ${descricao} - R$: ${valor.toFixed(2)}\n`
     resp1.innerText = `${resposta}----------------------------`
     resp2.innerText = `${numContas} Conta(s) - Total R$: ${valTotal.toFixed(2)}`
     
@@ -22,3 +22,8 @@ frm.addEventListener("submit", (e) => {       //"escuta" evento submit do form
     frm.inValor.value = ""
     frm.inDescricao.focus()              //posiciona no campo inDescricao do form
 })
+
+
+
+
+
