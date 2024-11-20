8ce24b7f736e3171ebe042b7a93ee6b41d275717
Allex Welter <allexwelter13@gmail.com>
2024-11-20 05:31:45 -0300

    Update ex5_10.js

--- cap05/nodejs/ex5_10.js
+++ cap05/nodejs/ex5_10.js
@@ -1,6 +1,5 @@
 //Elaborar um programa para uma loja que leia o valor de uma conta e o número de vezes que um cliente deseja parcelar esse valor (em boletos ou carnê). Para facilitar o troco, o lojista deseja que as parcelas iniciais não tenham centavos, ou seja, centavos apenas na última parcela. Informe como resposta o valor de cada parcela, considerando essa situação.
 
 
-
-
 const prompt = require("prompt-sync")                              //adiciona pacote prompt-sync
+const valor = Number(prompt("Valor R$: ")                         //lê valor do carnê
