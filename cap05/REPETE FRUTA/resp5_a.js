586c8bc4cb573f304e36f74aa056b2e73928567a
Allex Welter <allexwelter13@gmail.com>
2024-11-20 07:08:26 -0300

    Update resp5_a.js

--- cap05/REPETE FRUTA/resp5_a.js
+++ cap05/REPETE FRUTA/resp5_a.js
@@ -7,12 +7,40 @@ frm.addEventListener("submit", (e) => {
     e.preventDefault()
     const num = Number(frm.inNumero.value)
     const fruta = (frm.inFruta.value)
-    let estrelas = ""
+    let resposta = ""
     for(let i = 1; i <= num; i++) {
         if(i > 1) {
-            estrelas = estrelas + " * "
+            resposta = resposta + " * "
         }
-        estrelas = estrelas + fruta      
+        resposta = resposta + fruta      
     }
-    resp.innerText = estrelas 
+    resp.innerText = resposta 
 })
+
+
+
+
+/*
+const frm = document.querySelector("form") // obtém elementos da página
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {    // "escuta" evento submit do form
+  e.preventDefault() // evita envio do form
+
+  const fruta = frm.inFruta.value   // obtém dados do form
+  const num = Number(frm.inNumero.value)
+
+  // variável que vai acumular o número repetido das frutas
+  let resposta = ""
+
+  // cria uma repetição e...
+  for (let i = 1; i < num; i++) {
+    // ... a cada volta, adiciona uma fruta e o " * "
+    resposta = resposta + fruta + " * "
+  }
+
+  // a última fruta é acrescentada, sem o * no final
+  resp.innerText = resposta + fruta
+})
+
+*/
