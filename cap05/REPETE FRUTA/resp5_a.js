9cdba5cb2b34a490c305bec304b7d7ac0f480f84
Allex Welter <allexwelter13@gmail.com>
2024-11-20 06:58:34 -0300

    finish resp5_a

--- cap05/REPETE FRUTA/resp5_a.js
+++ cap05/REPETE FRUTA/resp5_a.js
@@ -1,1 +1,18 @@
 //Elaborar um programa que leia o nome de uma fruta e um número. O Programa deve repetir a exibição do nome da fruta, de acordo com o número informado. Utilize "*" para separar os nomes.
+
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const num = Number(frm.inNumero.value)
+    const fruta = (frm.inFruta.value)
+    let estrelas = ""
+    for(let i = 1; i <= num; i++) {
+        if(i > 1) {
+            estrelas = estrelas + " * "
+        }
+        estrelas = estrelas + fruta      
+    }
+    resp.innerText = estrelas 
+})
