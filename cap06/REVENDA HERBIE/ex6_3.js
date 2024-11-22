c029377e4e1df47d555d3c23d006505f3a4cdea4
Allex Welter <allexwelter13@gmail.com>
2024-11-22 16:20:45 -0300

    att ex6_3

---
+++ cap06/REVENDA HERBIE/ex6_3.js
@@ -0,0 +1,3 @@
+const frm = document.querySelector("form")     //obtém elementos da página
+const resp = document.querySelector("pre")
+const carros = []                             //declara vetor globar
