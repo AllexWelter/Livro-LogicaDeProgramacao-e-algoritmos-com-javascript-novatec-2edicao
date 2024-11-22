1cc0a7ad6e2a4c5bc4d20dab1ec1b9caefbec67e
Allex Welter <allexwelter13@gmail.com>
2024-11-22 17:26:14 -0300

    Update ex6_3.js

--- cap06/REVENDA HERBIE/ex6_3.js
+++ cap06/REVENDA HERBIE/ex6_3.js
@@ -7,8 +7,8 @@ frm.addEventListener("submit", (e) => {            //"escuta" evento submit do f
     const modelo = frm.inModelo.value            //obtém dados do form
     const preco = Number(frm.inPreco.value)
     carros.push({modelo, preco })                //adiciona dados ao vetor objeto 
-    frm.modelo.value = ""                      //limpa campos do form
-    frm.preco.value = ""
+    frm.inModelo.value = ""                      //limpa campos do form
+    frm.inPreco.value = ""
     inModelo.focus                           //posiciona cursor em inModelo
     //dispara um evento de click em btListar (equivale a um click no botão na página)
     frm.btListar.dispatchEvent(new Event("click"))
@@ -41,4 +41,20 @@ frm.btFiltrar.addEventListener('click', () => {
         lista += `${carro.modelo} - R$: ${carro.preco.toFixed(2)}\n`   
     }
     resp.innerText = `Carros até R$${maximo.toFixed(2)}\n${"-".repeat(40)}\n${lista}`
+})
+
+frm.btSimular.addEventListener("click", () => {
+    const desconto = Number(prompt("Qual o percentual do desconto: "))
+    if(desconto == 0 || isNaN(desconto)) {                               //se não informou ou valor inválido
+        return                                                          //... retorna
+    }
+    const carrosDesc = carros.map(aux => ({
+        modelo: aux.modelo,
+        preco: aux.preco - (aux.preco * desconto / 100)
+    }))
+    let lista = ""
+    for (const carro of carrosDesc) {                                //percorre cada elemento do array
+        lista += `${carro.modelo} - R$: ${carro.preco.toFixed(2)}\n`
+    }
+    resp.innerText = `Carros com Desconto: ${desconto}%\n${"-".repeat(40)}\n${lista}`
 })
