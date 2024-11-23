25c58b6efe4f53da40384e820427e0818c8b1724
Allex Welter <allexwelter13@gmail.com>
2024-11-23 07:22:10 -0300

    Update ex6_4.js

--- cap06/BRINQUEDOTECA/ex6_4.js
+++ cap06/BRINQUEDOTECA/ex6_4.js
@@ -6,5 +6,21 @@ frm.addEventListener("submit", (e) =>{
     e.preventDefault()                                    //evita envio do form
     const nome = frm.inNome.value                        //obtém conteúdo dos campos
     const idade = Number(frm.inIdade.value)
-    criancas.push({ nome, idade })                     //adiciona dados ao vetor de objetos 
+    criancas.push({ nome, idade })                     //adiciona dados ao vetor de objetos
+    frm.reset()                                       //limpa campos do form
+    frm.inNome.focus()                               //posiciona no campo do formulário
+    frm.btListar.dispacthEvent(new Event("click"))   //dispara um click em btListar 
+})
+
+frm.btListar.addEventListener("click", () => {
+    if(criancas.length == 0) {                        //se vazio, exibe alerta
+        alert("Não há crianças na lista")
+        return
+    }
+    let lista = ""                                   //para concatenar lista de crianças
+    for (const crianca of criancas) {
+        const { nome, idade } = crianca             //desestrutura objeto
+        lista += nome + " - " + idade + "anos\n" 
+    }
+    resp.innerText = lista                         //exibe a lista
 })
