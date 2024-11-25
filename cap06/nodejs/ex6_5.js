905b11ea76b2f8375794382b8d975b543fd021c9
Allex Welter <allexwelter13@gmail.com>
2024-11-25 08:57:11 -0300

    Update ex6_5.js

--- cap06/nodejs/ex6_5.js
+++ cap06/nodejs/ex6_5.js
@@ -1,3 +1,8 @@
 // a) Elaborar um programa que leia a nota de "n" alunos até o usuário digitar "Fim" no nome. Após, verifique e exiba a maior nota da turma. Se a maior nota for superior ou igual a 7, exiba os alunos que a obtiveram. Caso contrário, exiba a mensagem "Não há alunos em destque na turma".
 
 
+const prompt = require("prompt-sync")()
+
+console.log("Informe os alunos. Após, digite 'Fim' no nome para sair")
+const alunos = []                                                              //declara vetor
+break                                                                         //sai da repetição
