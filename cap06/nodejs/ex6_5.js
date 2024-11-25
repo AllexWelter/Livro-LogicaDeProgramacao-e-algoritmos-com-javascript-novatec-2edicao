04eef0c86bbf2cdf0caf2774924339c45b82add9
Allex Welter <allexwelter13@gmail.com>
2024-11-25 09:11:43 -0300

    Update ex6_5.js

--- cap06/nodejs/ex6_5.js
+++ cap06/nodejs/ex6_5.js
@@ -4,5 +4,20 @@
 const prompt = require("prompt-sync")()
 
 console.log("Informe os alunos. Após, digite 'Fim' no nome para sair")
-const alunos = []                                                              //declara vetor
-break                                                                         //sai da repetição
+const alunos = []                                                          //declara vetor
+do {
+    const nome = prompt("Nome: ")                                        //lê o nome
+    if(nome == "Fim") {                                                 //antes de ler a nota, verifica
+        break                                                          //sai da repetição
+    }
+const nota = Number(prompt("Nota: "))                                //lê a nota
+alunos.push({nome, nota})                                           //adiciona dados ao vetor de objetos
+console.log("Ok! Aluno(a) cadastrado(a)...")
+} while (true)
+console.log("-".repeat(40))                                            //exibe 40 -
+const maior = alunos.reduce((a, b) => Math.max(nota, b.nota), 0)      //obtém maior nota
+
+
+
+
+
