bdeb7a2fe8230a4aafb6c4507011f96bf9600f90
Allex Welter <allexwelter13@gmail.com>
2024-11-25 08:50:56 -0300

    update ex6_5

---
+++ cap06/nodejs/ex6_5.js
@@ -0,0 +1,3 @@
+// a) Elaborar um programa que leia a nota de "n" alunos até o usuário digitar "Fim" no nome. Após, verifique e exiba a maior nota da turma. Se a maior nota for superior ou igual a 7, exiba os alunos que a obtiveram. Caso contrário, exiba a mensagem "Não há alunos em destque na turma".
+
+
