cd272828c343eed42e4d7935bc273351622c2905
Allex Welter <allexwelter13@gmail.com>
2024-11-26 06:37:45 -0300

    Update ex6_7.js

--- cap06/nodejs/ex6_7.js
+++ cap06/nodejs/ex6_7.js
@@ -23,7 +23,7 @@ for (saque of saquesValidos) {
 }
 console.log("-".repeat(40))
 const totalSacado = saques.reduce((total, saque) => total + saque, 0)
-console.log(`Total dos saques: R$${totalSacado.toFixed(2)}`)
+console.log(`Total dos Saques: R$${totalSacado.toFixed(2)}`)
 
 const saquesInvalidos = saques.length - saquesValidos.length
 console.log(`\nNº de Tentativas de Saques (saques inválidos): ${saquesInvalidos}`)
