d17f9ea9b0e654e704c04ab3d4920a2fb9c5789f
Allex Welter <allexwelter13@gmail.com>
2024-11-26 06:21:27 -0300

    Update ex6_7.js

--- cap06/nodejs/ex6_7.js
+++ cap06/nodejs/ex6_7.js
@@ -1,1 +1,17 @@
 // c) Elaborar um programa que simule saques em um caixa eletrônico de um banco. Ler o valor solicitado por clientes até ser digitado 0. Sabendo que o caixa dispõe apenas de notas de 10, exiba após cada leitura se o saque é válido ou inválido. Ao final, listar os saques válidos e a soma dos saques. Exiba também o número de saques inválidos.
+
+const prompt = require("prompt-sync")()
+console.log("Informe o valor dos saques ou 0 para sair")
+const saques = []                                                 //declara vetor
+do {
+    const valor = Number(prompt("Saque R$: "))                   //lê valor
+    if (valor == 0) {
+        break                                                   //sai da repetição
+    }
+    saques.push(valor)                                            //adiciona ao vetor saques
+    if (valor % 10 == 0) {
+        console.log("Saque Realizado com Sucesso")
+    } else {
+        console.log("ERRO... Valor inválido (deve ser múltiplo de 10)")
+    }
+} while (true)
