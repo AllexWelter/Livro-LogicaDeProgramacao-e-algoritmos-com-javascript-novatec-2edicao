65d0e5217b69ba56da0e2b74e353005e2bd64238
Allex Welter <allexwelter13@gmail.com>
2024-11-27 09:12:30 -0300

    finish resp6_c

--- cap06/CONCURSO/resp6_c.js
+++ cap06/CONCURSO/resp6_c.js
@@ -2,39 +2,61 @@
 
 
 const frm = document.querySelector("form")
-const resp = document.querySelector("h3")
+const resp = document.querySelector("pre")
 const candidatos = []
 
-frm.addEventListener("submit", (e)=>{
+frm.addEventListener("submit", (e) => {
     e.preventDefault()
 
     const nome = frm.inNome.value
     const acertos = Number(frm.inAcertos.value)
-    candidatos.push({nome, acertos})                            //candidatos.push({nome: nome, acertos: acertos})
+    candidatos.push({ nome, acertos })                            //candidatos.push({nome: nome, acertos: acertos})
 
     frm.inNome.value = ""
     frm.inAcertos.value = ""
     frm.inNome.focus()
 
     frm.btListar.dispatchEvent(new Event("click"))           //dispara click em btListar
-
 })
 
-frm.btListar.addEventListener("click", () =>{
-    if(candidatos.length == 0) {
+frm.btListar.addEventListener("click", () => {
+    if (candidatos.length == 0) {
         alert("Não há candidatos na lista")
         return
     }
 
-   let lista = ""                                     //para concatenar lista de candidatos
-   for(const candidato of candidatos) {
-        lista += candidato.nome + " - " + candidato.acertos + "acertos\n"
-   }
-   resp.innerText = lista 
+    let lista = ""                                     //para concatenar lista de candidatos
+    for (const candidato of candidatos) {
+        lista += candidato.nome + " - " + candidato.acertos + " acertos\n"
+    }
+    resp.innerText = lista
 })
 
 frm.btAprovados.addEventListener("click", () => {
-    if(candidatos.length == 0) {
+    if (candidatos.length == 0) {
         alert("Não há candidatos na lista")
+        return
+    }
+
+    const corte = Number(prompt("Número de Acertos para Aprovação? "))
+    if (corte == 0 || isNaN(corte)) {
+        alert("Número inválido")
+        return
+    }
+
+    const copia = candidatos.slice()                      //cria uma cópia do vetor candidatos
+    copia.sort((a, b) => {a.acertos - b.acertos})      //ordena o vetor copia pelos acertos (regra para ficar crescente => a.acertos - b.acertos)
+    copia.reverse()                                   //inverte a ordem dos elementos (para ficar decrescente) 
+
+    let aprovados = ""                              //para concatenar aprovados
+    for(const candidato of copia) {
+        if(candidato.acertos >= corte) {
+            aprovados += candidato.nome + " - " + candidato.acertos + " acertos\n"
+        }
+    }
+    if(aprovados == "") {
+        resp.innerText = "Não há candidatos aprovados...."
+    } else {
+        resp.innerText = aprovados
     }
 })
