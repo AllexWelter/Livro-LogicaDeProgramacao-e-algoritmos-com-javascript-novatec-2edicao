0a9caec8f8b519adbd74e035dcc13c9ff2fba7dc
Allex Welter <allexwelter13@gmail.com>
2024-11-27 05:38:22 -0300

    update

--- cap06/JOGOS ELIMINATÓRIOS/resp6_a.js
+++ cap06/JOGOS ELIMINATÓRIOS/resp6_a.js
@@ -1,4 +1,4 @@
-// Elaborar  um programa para gerar uma tabela com os jogs de uma fase eliminatória de um campeonato. O programa deve conter três funções (a serem executadas no evento click de cada botão) para: 1) validar o preenchimento, adicionar um clube ao vetor e listar os clubes; 2) listar os clubes (se houver); 3) montar a tabela de jogos, no formato primeiro x último, segundo x penúltimo e assim por diante. Exibir mensagem e não listar a tabela de jogos, caso o número de clubes informados seja ímpar.
+// a) Elaborar  um programa para gerar uma tabela com os jogs de uma fase eliminatória de um campeonato. O programa deve conter três funções (a serem executadas no evento click de cada botão) para: 1) validar o preenchimento, adicionar um clube ao vetor e listar os clubes; 2) listar os clubes (se houver); 3) montar a tabela de jogos, no formato primeiro x último, segundo x penúltimo e assim por diante. Exibir mensagem e não listar a tabela de jogos, caso o número de clubes informados seja ímpar.
 
 const frm = document.querySelector("form")    // obtém elementos a serem manipulados
 const resp = document.querySelector("pre")
---
+++ cap06/NÚMEROS EM ORDEM/resp6_b.js
@@ -0,0 +1,1 @@
+// b) Elaborar um programa que adicione números a um vetor. O programa deve impedir a inclusão de números repetidos. Exibir a lista de números a cada inclusão. Ao clicar no botão verificar ordem, o programa deve analisar o conteúdo do vetor e informar se os números estão ou não em ordem crescente.
