f8d1c1fbeafd65474c2d49275aede49524e515f0
Allex Welter <allexwelter13@gmail.com>
2024-11-27 09:24:42 -0300

    att

--- cap06/NÚMEROS EM ORDEM/resp6_b.js
+++ cap06/NÚMEROS EM ORDEM/resp6_b.js
@@ -1,8 +1,8 @@
 // b) Elaborar um programa que adicione números a um vetor. O programa deve impedir a inclusão de números repetidos. Exibir a lista de números a cada inclusão. Ao clicar no botão verificar ordem, o programa deve analisar o conteúdo do vetor e informar se os números estão ou não em ordem crescente.
 
 const frm = document.querySelector("form")
-const resp1 = document.querySelector("#outResp1")
-const resp2 = document.querySelector("#outResp2")
+const resp1 = document.querySelector("#outLista")
+const resp2 = document.querySelector("#outOrdem")
 const numeros = []
 
 frm.addEventListener("submit", (e) =>{
