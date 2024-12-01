eb5ff028e526227fc6da669c1cd82b683713e4ff
Allex Welter <allexwelter13@gmail.com>
2024-12-01 10:21:35 -0300

    Update extra1_1.js

--- cap06/EXERCÍCIOS EXTRAS (fora do livro)/ESTOQUE DE PRODUTOS/extra1_1.js
+++ cap06/EXERCÍCIOS EXTRAS (fora do livro)/ESTOQUE DE PRODUTOS/extra1_1.js
@@ -11,7 +11,6 @@ Listar produtos com estoque zerado: Ao clicar em um botão, listar todos os prod
     Caso não haja produtos com estoque zero, exibir uma mensagem apropriada.
 */
 
-
 const frm = document.querySelector("form")
 const resp1 = document.querySelector("#outResp1")
 const resp2 = document.querySelector("#outResp2")
@@ -21,9 +20,15 @@ const produtos = []
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
     
-    const nome = frm.inNome.value
+    const nome = frm.inNome.value.trim()     //remove espaços desnecessários
     const num = Number(frm.inQuantidade.value)
-    produtos.push({nome, num})           //objeto deve ser declado entre {}
+
+    const existente = produtos.find(produto => produto.nome.toLowerCase() === nome.toLowerCase())
+        if(existente) {
+            existente.num += num            //atualiza a quantidade do produto existente
+        } else {
+            produtos.push({nome, num})      //adiciona um novo produto 
+        }
     
     frm.inNome.value = ""
     frm.inQuantidade.value = ""
@@ -34,20 +39,48 @@ frm.addEventListener("submit", (e) => {
 
 frm.btListar.addEventListener("click", () =>{
     if(produtos.length == 0) {
-        alert("Favor inserir dados!")
+        resp1.innerText = "Nenhum produto no estoque."
+        resp2.innerText = " "  //Limpa qualquer mensagem anterior
         return
     }
-    let lista = ""
-    for(const produto of produtos) {
-        lista += produto.nome + " -> " + produto.num + "\n"
+
+    let lista = "Lista Geral de Produtos:\n"
+    produtos.forEach(produto =>  {
+        lista += `${produto.nome} -> ${produto.num}\n`
+    })
+    resp1.innerText = lista                      //exibe lista de produtos no primeiro <pre>
+    
+    const zerados = produtos.filter(produto => produto.num === 0)   //filtra produtos com quantidade 0 
+
+    if(zerados.length === 0) {
+        resp2.innerText = "Não há produtos com estoque zerado."
+    } else {
+        let listaZerados = "Produtos com estoque zerado:\n"
+        zerados.forEach(produto => {
+            listaZerados += produto.nome + "\n"
+        })
+        resp2.innerText = listaZerados
     }
-    resp1.innerText = lista    
 })
 
 frm.btRemover.addEventListener("click", () =>{
+    const nome = frm.inNome.value.trim()
+
     if(produtos.length == 0) {
         alert("Produto não localizado ou não existe produto para remoção!")
+        return
     }
 
+    const indice = produtos.findIndex(produto => produto.nome.toLowerCase() === nome.toLowerCase())      //Procura o índice do produto no array
+
+    if(indice === -1) {
+        alert("Produto não encontrado.")
+    } else {
+        produtos.splice(indice, 1)     //remove produto pelo índice
+        alert(`Produto ${nome} removido com sucesso.`)
+        frm.btListar.dispatchEvent(new Event("click"))  //atualiza a lista 
+    }
+    frm.inNome.value = ""
+    frm.inNome.focus()
 
 })
