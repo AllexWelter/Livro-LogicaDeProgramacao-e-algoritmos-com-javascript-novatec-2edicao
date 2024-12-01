3b44448c097801452128e2454f21f4d80866d246
Allex Welter <allexwelter13@gmail.com>
2024-12-01 07:24:51 -0300

    update

---
+++ cap06/EXERCÍCIOS EXTRAS (fora do livro)/ESTOQUE DE PRODUTOS/extra1_1.js
@@ -0,0 +1,17 @@
+/*
+//Desenvolva um programa que gerencie um estoque de produtos em uma loja. O programa deve ter as seguintes funcionalidades:
+Adicionar Produto: O programa deve permitir que o usuário insira o nome do produto e quantidade inicial.
+    Caso o nome já exista, deve apenas atualizar a quantidade do produto
+    Exibir a lista atualizada após cada inclusão ou atualização.
+
+Remover produto: Deve permitir que o usuário remova produtos do estoque com base no nome informado.
+    Caso o nome não seja encontrado, exibir uma mensagem de erro.
+
+Listar produtos com estoque zerado: Ao clicar em um botão, listar todos os produtos cuja quantidade seja igual a zero.
+    Caso não haja produtos com estoque zero, exibir uma mensagem apropriada.
+
+
+
+
+
+*/
