2349e71e2bbcf17db4bf339840a980a865d97fc4
Allex Welter <allexwelter13@gmail.com>
2024-12-02 08:21:37 -0300

    Update extra1_2.js

--- cap06/EXERCÍCIOS EXTRAS (fora do livro)/LISTA DE ALUNOS/extra1_2.js
+++ cap06/EXERCÍCIOS EXTRAS (fora do livro)/LISTA DE ALUNOS/extra1_2.js
@@ -2,10 +2,20 @@ const frm = document.querySelector("form")
 const resp1 = document.querySelector("#outResp1")
 const resp2 = document.querySelector("#outResp2")
 const resp3 = document.querySelector("#outResp3")
-
+const alunos = []
 
 frm.addEventListener("submit", (e) =>{
     e.preventDefault()
 
     const aluno = frm.inAluno.value
+    alunos.push = (aluno)
+
+    frm.bListar.dispatchEvent(new Event("click"))
+
+    frm.inAluno.value = ""
+    frm.inAluno.focus()
+})
+
+frm.bListar.addEventListener("click", () =>{
+    
 })
