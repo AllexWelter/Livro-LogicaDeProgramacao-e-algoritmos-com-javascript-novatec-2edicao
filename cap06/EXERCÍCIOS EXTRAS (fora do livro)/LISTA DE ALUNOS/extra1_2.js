23c9824980fa6a7a8895de31845335ec60eb1ef2
Allex Welter <allexwelter13@gmail.com>
2024-12-02 08:01:34 -0300

    Update extra1_2.js

---
+++ cap06/EXERCÍCIOS EXTRAS (fora do livro)/LISTA DE ALUNOS/extra1_2.js
@@ -0,0 +1,11 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+const resp3 = document.querySelector("#outResp3")
+
+
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const aluno = frm.inAluno.value
+})
