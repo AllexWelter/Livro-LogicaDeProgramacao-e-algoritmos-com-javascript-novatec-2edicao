2a99a129699e1a31a94f944730fae00df7f14c19
Allex Welter <allexwelter13@gmail.com>
2024-12-03 07:49:53 -0300

    Update ex7_8.js

--- cap07/CAIXA DA LOJA/ex7_8.js
+++ cap07/CAIXA DA LOJA/ex7_8.js
@@ -16,4 +16,23 @@ frm.addEventListener("submit", (e) =>{
 
     const partes = dataVenc.split(" - ")      //data vem no formato aaaa-mm-dd
     vencto.setDate(Number(partes[2]))
+    vencto.setMonth(Number(partes[1]) - 1)
+    vencto.setFullYear(Number(partes[0]))
+
+    const atraso = hoje - vencto             //calcula a diferença de dias entre datas (em ms)
+    let multa = 0                            //incializa multa e juros com 0 
+    let juros = 0
+
+    if(atraso > 0) {                         //se conta estiver em atraso...
+        //converte ms do atraso em dias (1 dia = 24 x 60min x 60seg x 1000ms: 86400000)
+        const dias = atraso / 8640000
+        multa = valor * TAXA_MULTA           //calcula multa e juros
+        juros = valor * TAXA_JUROS * dias
+
+        const total = valor + multa + juros       //calcula o total
+
+
+        frm.outMulta.value
+    }
+
 })
