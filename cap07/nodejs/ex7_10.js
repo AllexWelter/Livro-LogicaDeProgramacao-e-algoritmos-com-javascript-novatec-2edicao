b37eef864d0dcf70444bdb02d9078f4ca7a9511f
Allex Welter <allexwelter13@gmail.com>
2024-12-03 16:16:39 -0300

    Update ex7_10.js

--- cap07/nodejs/ex7_10.js
+++ cap07/nodejs/ex7_10.js
@@ -2,7 +2,7 @@
 b) Elaborar um programa que leia a altura de uma árvore (número de linhas) e após exiba a árvore iniciando com 2 estrela (asteriscos) e aumentando em 2 a cada linha. Fazer com que a árvores tenha uma margem esquerda fixa de 30 espaços e fique centralizada.
 */
 
-const prompt = require("prompt-sync")    //pacote para entrada de dados
+const prompt = require("prompt-sync")()    //pacote para entrada de dados
 
 const altura = Number(prompt("Altura da árvore: "))    //lê o número de linhas (altura)
 console.log()                                          //deixa uma linha em branco
