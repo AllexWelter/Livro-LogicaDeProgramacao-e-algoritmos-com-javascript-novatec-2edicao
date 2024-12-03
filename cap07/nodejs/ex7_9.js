2e1a6d30da844cb0299ec8d3c95ca38713a49dca
Allex Welter <allexwelter13@gmail.com>
2024-12-03 15:36:33 -0300

    Update ex7_9.js

---
+++ cap07/nodejs/ex7_9.js
@@ -0,0 +1,3 @@
+/*
+a) Elaborar um programa que leia uma fórmula matemática e enalise se os parênteses na fórmula estão corretos. A análise deve considerar dois fatores: o número de "(" deve ser igual ao número de ")" e, ao ler a fórmula da esquerda para a direita, em nenhum momento, o número de ")" pode ser maior que o número de "(", ou seja, não pode fechar um parêntese sem antes ter aberto.
+*/
