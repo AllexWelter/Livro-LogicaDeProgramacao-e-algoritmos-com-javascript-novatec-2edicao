06ec2a56d2bf7ba185f64da414e3b3e3f0155a5e
Allex Welter <allexwelter13@gmail.com>
2024-12-04 06:04:02 -0300

    att

---
+++ cap07/CRIPTOGRAFIA/resp7_a.js
@@ -0,0 +1,3 @@
+/*
+a) Você deve desenvolver um programa de criptografia para transmitir mensagens entre amigos. O programa deve ler uma mensagem e, em seguida, exibi-la criptografada. A criptografia consiste em: a) exibir todas as letras das posições pares da mensagem; b) exibir todas as letras das posições ímpares da mensagem. O programa deve conter ainda um botão para decriptografar a mensagem, ou seja, retornar a mensagem original a partir do texto cifrado.
+*/
