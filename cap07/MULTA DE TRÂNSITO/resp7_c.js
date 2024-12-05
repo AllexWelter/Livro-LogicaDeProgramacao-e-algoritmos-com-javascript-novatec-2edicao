336707effffa474b3d726be3e0d56faebfb1d9ff
Allex Welter <allexwelter13@gmail.com>
2024-12-05 05:43:48 -0300

    att

---
+++ cap07/MULTA DE TRÂNSITO/resp7_c.js
@@ -0,0 +1,3 @@
+/*
+c) Suponha que o prazo para o pagamento de uma infração de trânsito com desconto seja de 90 dias. Elaborar um programa que leia a data de uma infração e o valor da multa. Informe queal a data limite do pagamento com desconto (até 90 dias) e o valor a ser pago até essa data (com 20% de desconto).
+*/
