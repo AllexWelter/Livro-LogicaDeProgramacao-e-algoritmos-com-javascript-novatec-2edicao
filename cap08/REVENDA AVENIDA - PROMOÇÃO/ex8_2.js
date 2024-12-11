99d003cff30e94ee8512ee13efa72abb96485be4
Allex Welter <allexwelter13@gmail.com>
2024-12-11 06:51:14 -0300

    Update ex8_2.js

--- cap08/REVENDA AVENIDA - PROMOÇÃO/ex8_2.js
+++ cap08/REVENDA AVENIDA - PROMOÇÃO/ex8_2.js
@@ -5,11 +5,13 @@ const resp3 = document.querySelector("outResp3")
 
 
 frm.addEventListener("submit", (e) =>{
-    e.preventDefault()
+    e.preventDefault()  //envia envio do form
 
-    const modelo = frm.inModelo.value
+    const modelo = frm.inModelo.value     //obtém o conteúdo dos campos
     const ano = Number(frm.inAno.value)
     const preco = Number(frm.inPreco.value)
-    const classificacao = 
+    const classificacao = classificarVeiculo(ano)             //chama funções e atribui
+    const entrada = calcularEntrada(preco, classificacao)     //... retorno às variáveis
+    const parcela = (preco - entrada) / 10                    //usa retorno da função para cálculo
 
 })
