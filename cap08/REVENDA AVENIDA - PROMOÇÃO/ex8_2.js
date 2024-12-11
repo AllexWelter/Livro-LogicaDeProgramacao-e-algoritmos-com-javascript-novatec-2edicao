89c8f6ce4315a51424b69ce7462f953160f8e07a
Allex Welter <allexwelter13@gmail.com>
2024-12-11 06:41:16 -0300

    Update ex8_2.js

--- cap08/REVENDA AVENIDA - PROMOÇÃO/ex8_2.js
+++ cap08/REVENDA AVENIDA - PROMOÇÃO/ex8_2.js
@@ -1,4 +1,4 @@
-const frm = document.querySelector("form")
+const frm = document.querySelector("form")              //obtém os elementos da página
 const resp1 = document.querySelector("outResp1")
 const resp2 = document.querySelector("outResp2")
 const resp3 = document.querySelector("outResp3")
@@ -6,5 +6,10 @@ const resp3 = document.querySelector("outResp3")
 
 frm.addEventListener("submit", (e) =>{
     e.preventDefault()
-    
+
+    const modelo = frm.inModelo.value
+    const ano = Number(frm.inAno.value)
+    const preco = Number(frm.inPreco.value)
+    const classificacao = 
+
 })
