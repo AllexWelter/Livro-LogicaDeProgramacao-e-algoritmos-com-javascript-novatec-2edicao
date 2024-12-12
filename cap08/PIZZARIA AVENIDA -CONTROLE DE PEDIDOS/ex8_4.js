e72151299ab9d39d4a084c621bcabd8ef3efa360
Allex Welter <allexwelter13@gmail.com>
2024-12-12 07:25:21 -0300

    Update ex8_4.js

--- cap08/PIZZARIA AVENIDA -CONTROLE DE PEDIDOS/ex8_4.js
+++ cap08/PIZZARIA AVENIDA -CONTROLE DE PEDIDOS/ex8_4.js
@@ -2,3 +2,8 @@ const frm = document.querySelector("form")     //obtém os elementos da página
 const resp = document.querySelector("pre")
 
 const itens = []      //vetor global para armazenar os itens do pedido
+
+
+frm.rbPizza.addEventListener("click", () => {
+    
+})
