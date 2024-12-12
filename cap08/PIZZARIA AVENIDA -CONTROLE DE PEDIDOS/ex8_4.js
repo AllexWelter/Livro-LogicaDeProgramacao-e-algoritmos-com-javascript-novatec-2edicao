0c6760705817f301e1a9ddd4c9b39374d8f9ec05
Allex Welter <allexwelter13@gmail.com>
2024-12-12 07:21:10 -0300

    Update ex8_4.js

--- cap08/PIZZARIA AVENIDA -CONTROLE DE PEDIDOS/ex8_4.js
+++ cap08/PIZZARIA AVENIDA -CONTROLE DE PEDIDOS/ex8_4.js
@@ -1,2 +1,4 @@
-const frm = document.querySelector("form")
+const frm = document.querySelector("form")     //obtém os elementos da página
 const resp = document.querySelector("pre")
+
+const itens = []      //vetor global para armazenar os itens do pedido
