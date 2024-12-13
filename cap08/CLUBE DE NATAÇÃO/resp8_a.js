6c4561d29d7c160416214188090306665c04656c
Allex Welter <allexwelter13@gmail.com>
2024-12-13 08:15:31 -0300

    Update resp8_a.js

---
+++ cap08/CLUBE DE NATAÇÃO/resp8_a.js
@@ -0,0 +1,7 @@
+/* a) Elaborar um programa que leia o nome e a idade de um atleta de um clube de natação. O programas deve exibir o nome com " - " abaixo das letras do nome e a categoria do atleta, que pode ser "infantil" (até 12 anos), "Juvenil" (entre 13 e 18 anos) ou "Adulto" (acima de 18 anos). O programa deve conter as funções:
+
+    * retornarTracos() - que receba um nome como parâmetro e retorne uma linha com " - " para as letras do nome (nos espaços, manter os epaços).
+
+    * categorizarAluno() - que receba um número como parâmetro e retorne a categoria do aluno, conforme indicação no enunciado do programa.
+
+*/
