db36eb757cffa7569d977551d71baab10c741f65
Allex Welter <allexwelter13@gmail.com>
2024-12-13 05:52:43 -0300

    Update ex8_5.js

--- cap08/nodejs/ex8_5.js
+++ cap08/nodejs/ex8_5.js
@@ -1,1 +1,10 @@
 const prompt = require("prompt-sync")()     //adiciona o pacote ao programa
+
+const vinhos = []      
+
+function titulo(texto) {                   //recebe, por parâmetro, o texto a ser exibido
+    console.log()
+    console.log(texto)
+    console.log("=".reapeat(40))
+
+}
