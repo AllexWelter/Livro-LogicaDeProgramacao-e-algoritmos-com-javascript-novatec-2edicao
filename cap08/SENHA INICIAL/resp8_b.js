796656d9686573a1b95a71aa050c2a73d5c0724d
Allex Welter <allexwelter13@gmail.com>
2024-12-16 07:13:15 -0300

    Update resp8_b.js

---
+++ cap08/SENHA INICIAL/resp8_b.js
@@ -0,0 +1,5 @@
+/*b) Elaborar um programa que leia o nome completo de um aluno. O programa deve validar o preenchimento de um nome completo e exibir a senha inicial do aluno, a qual será o sobrenome seguido pelo número de vogais do nome completo dele. O programa de conter as seguintes funções:
+
+    * 
+
+*/
