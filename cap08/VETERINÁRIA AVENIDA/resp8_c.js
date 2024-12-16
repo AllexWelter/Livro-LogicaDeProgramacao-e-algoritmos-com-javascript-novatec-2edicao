ebdc19567c7958280eb874288aa7ba31be4cec6f
Allex Welter <allexwelter13@gmail.com>
2024-12-16 16:45:05 -0300

    Update resp8_c.js

--- cap08/VETERINÁRIA AVENIDA/resp8_c.js
+++ cap08/VETERINÁRIA AVENIDA/resp8_c.js
@@ -1,4 +1,4 @@
-/* c) Elaborar um programa para um veterinária, o qual leia o preço de uma vacina e se o cliente possui ou não convênio. Caso possua algum convênio, exibir uma caixa de seleção com os convênios "Amigos dos Animais" e "Saúde Animal". O programa deve exibir o valor do desconto (10% sem convênio; 20% para "Amigos dos Animais"; 50% para "Saúde Animal") e o valor a ser pago. Criar a função:
+/* c) Elaborar um programa para uma veterinária, o qual leia o preço de uma vacina e se o cliente possui ou não convênio. Caso possua algum convênio, exibir uma caixa de seleção com os convênios "Amigos dos Animais" e "Saúde Animal". O programa deve exibir o valor do desconto (10% sem convênio; 20% para "Amigos dos Animais"; 50% para "Saúde Animal") e o valor a ser pago. Criar a função:
 
     * calcularDesconto() - que receba os parâmetros valor e taxa de desconto. Retornar o valor do desconto.
 
