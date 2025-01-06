97d3a1366ea40796f2051d2043d7e7f94c06dcba
Allex Welter <allexwelter13@gmail.com>
2025-01-06 09:24:42 -0300

    Update ex9_1.js

--- cap09/js/ex9_1.js
+++ cap09/js/ex9_1.js
@@ -34,6 +34,17 @@ frm.rbFarroupilha.addEventListener("change", trocarClube)
 
 const verificarClube = () => {
     if(localStorage.getItem("clube")) {                   //se já estiver salvo algum clube
+        const clube = localStorage.setItem("clube")       //obtém o nome do clube
 
+        if (clube == "Brasil") {                          //conforme o clube, marca checked
+            frm.rbBrasil.checked = true
+        } else if (clube == "Pelotas") {
+            frm.rbPelotas.checked = true
+        } else {
+            frm.rbFarroupilha.checked = true 
+        }
+        trocarClube()                                    //chama function que troca imagem e cores 
     }
 }
+// ao carregar a página, verifica se cliente já selecionou clube anteriormente
+window.addEventListener("load", verificarClube)
