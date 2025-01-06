f2aab98b63b72a690c9c0407ceea12fad46d9d1d
Allex Welter <allexwelter13@gmail.com>
2025-01-06 08:46:55 -0300

    add archives

---
+++ cap09/js/ex9_1.js
@@ -0,0 +1,8 @@
+const frm = document.querySelector("form")
+const imClube = document.querySelector("#imgClube")
+const dvTitulo = document.querySelector("#divTitulo")
+
+
+const trocarClube = () => {
+    let clube                   //variável que irá receber o nome do clube 
+}
