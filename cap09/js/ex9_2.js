a22e63c327d0da0aee524bc4bf38f003776a19c1
Allex Welter <allexwelter13@gmail.com>
2025-01-07 08:24:52 -0300

    Update ex9_2.js

--- cap09/js/ex9_2.js
+++ cap09/js/ex9_2.js
@@ -9,26 +9,27 @@ const trocarClube = () => {
     const clubes = ["Brasil", "Pelotas", "Farroupilha"]   //vetor com a lista de clubes
 
     let selecao
-// percorre os inRadios para verificar qual está selecionado
-for (let i = 0; i < inRadios; i++) {
-    if (inRadios[i].checked) {
-        selecao = i                                       //se selecionado, armazena índice do radio selecionado
-        break                                             //e sai da repetição
+    // percorre os inRadios para verificar qual está selecionado
+    for (let i = 0; i < inRadios; i++) {
+        if (inRadios[i].checked) {
+            selecao = i                                       //se selecionado, armazena índice do radio selecionado
+            break                                             //e sai da repetição
+        }
     }
-}  
 
-if (selecao <= 2) {                                       //se selecao <= 2, então torce por algum clube
-    dvTitulo.className = `row cores-${clubes[selecao]}`   //modifica a cor
-    //muda a propriedade src com a imagem do clube selecionado
-    imClube.src = `img/${clubes[selecao].toLowerCase()}.png`
-    imClube.className = "img-fluid"                       //muda estilo para exibir imagem
-    imClube.alt = `Símbolo do ${clubes[selecao]}`         //texto alternativo
-    localStorage.setItem("clube", clubes[selecao])        //salva nome do clube
-} else {                                                  //se selecionou nenhum
-    dvTitulo.className = "row"                            //tira a classe de cor de divTitulo
-    imClube.className = "d-none"                          //oculta a imagem
-    imClube.alt = ""                                      //limpa texto alternativo
-    localStorage.removeItem("clube")                      //remove variável do localStorage
+    if (selecao <= 2) {                                       //se selecao <= 2, então torce por algum clube
+        dvTitulo.className = `row cores-${clubes[selecao]}`   //modifica a cor
+        //muda a propriedade src com a imagem do clube selecionado
+        imClube.src = `img/${clubes[selecao].toLowerCase()}.png`
+        imClube.className = "img-fluid"                       //muda estilo para exibir imagem
+        imClube.alt = `Símbolo do ${clubes[selecao]}`         //texto alternativo
+        localStorage.setItem("clube", clubes[selecao])        //salva nome do clube
+    } else {                                                  //se selecionou nenhum
+        dvTitulo.className = "row"                            //tira a classe de cor de divTitulo
+        imClube.className = "d-none"                          //oculta a imagem
+        imClube.alt = ""                                      //limpa texto alternativo
+        localStorage.removeItem("clube")                      //remove variável do localStorage
+    }
 }
 
 const verificarClube = () => {
@@ -42,8 +43,9 @@ const verificarClube = () => {
         } else {
             inRadios[2].checked = true 
         }
-        trocarClube
+        trocarClube()                                     // chama a function que troca a image e cores       
     }
 }
 
-}
+//ao carregar a página, verifica se cliente já selecionou clube anteriormente
+window.addEventListener("load", verificarClube)
