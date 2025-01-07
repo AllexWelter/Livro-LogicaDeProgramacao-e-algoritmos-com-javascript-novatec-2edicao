5badad5dfa90f628734fd36995d95bac4c04e6b6
Allex Welter <allexwelter13@gmail.com>
2025-01-07 07:20:28 -0300

    Update ex9_2.js

--- cap09/js/ex9_2.js
+++ cap09/js/ex9_2.js
@@ -1,10 +1,16 @@
 const inRadios = document.querySelectorAll("input")                //captura as tags input da página
 
-//percorre os elementos para associar function ao evento change
+// percorre os elementos para associar function ao evento change
 for (const inRadio of inRadios) {
     inRadio.addEventListener("change", trocarClube)
 }
 
 const trocarClube = () => {
+    const clubes = ["Brasil", "Pelotas", "Farroupilha"]   //vetor com a lista de clubes
 
+    let selecao
+// percorre os inRadios para verificar qual está selecionado
+for (let i = 0; i < inRadios; i++) {
+    if (inRadios[i].checked)
+}  
 }
