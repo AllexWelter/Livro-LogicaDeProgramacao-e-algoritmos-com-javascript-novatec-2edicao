089c82b3f40252de1d0d1229242a11b8d28b0d2d
Allex Welter <allexwelter13@gmail.com>
2025-01-07 07:13:20 -0300

    Update ex9_2.js

--- cap09/js/ex9_2.js
+++ cap09/js/ex9_2.js
@@ -1,1 +1,6 @@
 const inRadios = document.querySelectorAll("input")                //captura as tags input da página
+
+//percorre os elementos para associar function ao evento change
+for (const inRadio of inRadios) {
+    inRadio.addEventListener("change", trocarClube)
+}
