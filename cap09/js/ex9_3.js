f5721681f968b02937d795b49cb0035175963118
Allex Welter <allexwelter13@gmail.com>
2025-01-20 07:36:52 -0300

    Update ex9_3.js

--- cap09/js/ex9_3.js
+++ cap09/js/ex9_3.js
@@ -1,2 +1,14 @@
 const frm = document.querySelector("form")         //obtém os elementos da página
 const repLista = document.querySelector("pre")
+
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()                           //evita envio do form
+
+    const nome = frm.inNome.value                //conteúdo do campo nome
+    const peso = Number(frm.inPeso.value)        //conteúdo do campo peso (em número)
+
+
+ //chama function que verifica se peso já foi apostado   
+    
+})
