14a113658067a7f0acb7ac826b88f1735ef374fa
Allex Welter <allexwelter13@gmail.com>
2025-01-21 15:00:36 -0300

    Update resp9_b.js

--- cap09/COMPRA DA SEMANA/resp9_b.js
+++ cap09/COMPRA DA SEMANA/resp9_b.js
@@ -6,4 +6,34 @@ frm.addEventListener("submit", (e) =>{
 
     const produto = frm.inProduto.value
 
+    //se houver dados salvo
+    if (localStorage.getItem("comprasProduto")) {
+        //obtém itens separando-os em elemento de vetor
+        const produtos = localStorage.getItem("comprasProduto").split(";")
+
+        //adiciona o novo produto ao vetor
+        produtos.push(produto)
+
+        //classifica o vetor
+        produtos.sort()
+
+        //salva vetor classificado (com elementos separados por "")
+        localStorage.setItem("comprasProduto", produtos.join(";"))
+    }else {
+        //senão (é a primeira inclusão), salva apenas o produto
+        localStorage.setItem("compraProduto", produto)
+    }
+
+    listarProdutos()
+
+    frm.reset()             //ou frm.inProduto.value = ""
+    frm.inProduto.focus()
+
 })
+
+const listarProdutos = () => {
+    if(!localStorage.getItem("comprasProduto")) {
+        resp.innerText = ""
+        return
+    }    
+}
