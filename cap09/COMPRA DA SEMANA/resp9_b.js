3294c143d1ff60391679a2e8c8ad890da2559a54
Allex Welter <allexwelter13@gmail.com>
2025-01-21 14:49:05 -0300

    Update resp9_b.js

--- cap09/COMPRA DA SEMANA/resp9_b.js
+++ cap09/COMPRA DA SEMANA/resp9_b.js
@@ -1,3 +1,9 @@
 const frm = document.querySelector("form")
 const resp = document.querySelector("pre")
 
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault
+
+    const produto = frm.inProduto.value
+
+})
