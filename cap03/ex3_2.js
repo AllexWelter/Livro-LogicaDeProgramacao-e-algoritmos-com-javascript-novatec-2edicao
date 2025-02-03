a9878e22ca67b2575302564e0bb129761c2a75e6
Allex Welter <allexwelter13@gmail.com>
2025-02-03 06:40:20 -0300

    Update ex3_2.js

--- cap03/ex3_2.js
+++ cap03/ex3_2.js
@@ -4,6 +4,7 @@ const preco = Number(prompt("Preço R$: "))
 
 const entrada = preco * 0.50                //calcula valor de entrada
 const parcela = (preco * 0.50) / 12         //... e das parcelas
+
 console.log(`Promoção : ${veiculo}`)        //exibe as respostas
 console.log(`Entrada de R$: ${entrada.toFixed(2)}`)
 console.log(`+ 12x de R$: ${parcela.toFixed(2)}`)
